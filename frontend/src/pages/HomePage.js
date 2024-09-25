db402f8f1cab1d99006643f6430fa17f68dbe0d5
Abhishek Kumar <abhishek.kumar.2301.vivek@gmail.com>
2024-09-25 15:13:16 +0530

    frontend

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -12,36 +12,31 @@ const HomePage = () => {
   return (
     <div className="min-h-screen bg-[#01161B] text-gray-300">
       {/* Hero Section */}
-      <section className=" text-white py-20 h-screen flex items-center justify-center">
+      <section className="text-white py-[10vh] h-screen flex items-center justify-center mt-[4vh]">
         <div className="container mx-auto flex items-center">
-          {/* <!-- Left side: Centered text and button --> */}
-          <div className="w-1/2 flex flex-col items-center justify-center p-8">
-            <h2 className="text-xl font-bold mb-4 text-center hero-heading">
-              Your Rights, Your Power.
+          <div className="w-1/2 flex flex-col items-center justify-center">
+            <h2 className="text-[4vw] font-bold mb-4 text-center hero-heading">
+              Your Rights,<br/> Your Power.
             </h2>
-            <p className="text-lg mb-6 text-center hero-text">
-            <span className="darking opacity-50">An</span> One-Stop Learning Solution <span className="darking opacity-50">about<br/>
-            the </span>Laws and Rights <span className="darking opacity-50">that protect you.</span>
+            <p className="text-[2vw] mb-6 text-center hero-text">
+              <span className="darking opacity-50">An</span> One-Stop Learning Solution <span className="darking opacity-50">about<br/>
+              the </span>Laws and Rights <span className="darking opacity-50">that protect you.</span>
             </p>
             <Link
               to="/awareness"
-              className="inline-block bg-white text-blue-600 px-8 py-3 rounded-lg font-semibold hero-button"
-            ><div className="hero-button-text text-center">Get Started 
-            {/* <img
-                src={arrowIcon}
-              /> */}
-            </div>
-              
+              className="inline-block bg-white text-blue-600 px-[2vw] py-[1vw] rounded-lg font-semibold hero-button"
+            >
+              <div className="hero-button-text text-center">
+                Get Started
+              </div>
             </Link>
           </div>
-
-          {/* <!-- Right side: Image --> */}
-          <div className="w-1/2 h-screen flex items-center">
-            <div className="relative h-full w-auto overflow-hidden shadow-lg -mr-20">
+          <div className="w-1/2 h-screen flex items-end">
+            <div className="relative w-full max-h-full overflow-hidden">
               <img
                 src={emblemImage}
                 alt="Legal knowledge"
-                className="h-full w-auto object-cover"
+                className="w-full max-h-[90vh] object-contain"
               />
             </div>
           </div>
@@ -51,11 +46,9 @@ const HomePage = () => {
       {/* Preamble of Indian Constitution */}
       <section className="bg-black">
         <PreambleSection />
- 
 
-      {/* News Section of Page */}
-      <img src={line} className="lines mx-auto"></img>
-   
+        {/* News Section of Page */}
+        <img src={line} className="lines mx-auto"></img>
         <NewsSection />
         <img src={line} className="lines mx-auto mt-12"></img>
       </section>
@@ -64,7 +57,7 @@ const HomePage = () => {
       <section>
         <FeaturesSection />
       </section>
-      
+
       {/* About Us Section */}
       <section className="z-100">
         <AboutUs />
@@ -76,9 +69,9 @@ const HomePage = () => {
       </section>
 
       {/* Footer */}
-      <footer className="bg-gray-900 text-gray-300 py-4">
+      <footer className="bg-gray-900 text-gray-300 py-[2vh]">
         <div className="container mx-auto text-center">
-          <p>
+          <p className="text-[1.2vw]">
             &copy; {new Date().getFullYear()} Law Aware. All rights reserved.
           </p>
         </div>
